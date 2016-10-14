ab46e84e7eef33d879f07080b659e7cfad0b6f5c
Aleksandra Wisniewska <wisniewska.aleks@gmail.com>
2016-10-14 10:26:12 +0100

    add Sunday pub date

--- config/article.js
+++ config/article.js
@@ -9,7 +9,7 @@ export default () => ({ // eslint-disable-line
 
   // To set an exact publish date do this:
   //       new Date('2016-05-17T17:11:22Z')
-  publishedDate: new Date(),
+  publishedDate: new Date('2016-10-16T12:11:22Z'),
 
   headline: 'Trump\'s Russian connections',
 
